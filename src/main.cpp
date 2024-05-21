6bc969e6aacd0d8d7d71c07e62913e16d3c18b5d
ktnlvr <artur.roos.artur@gmail.com>
2024-05-21 17:05:31 +0300

    Trigger a workflow

--- src/main.cpp
+++ src/main.cpp
@@ -12,25 +12,23 @@
 #include "renderer/renderer.hpp"
 #include "renderer/vertex.hpp"
 
-const char *vertex_shader_source =
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 aColor;\n"
-    "out vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos, 1.0);\n"
-    "   ourColor = aColor;\n"
-    "}\0";
+const char *vertex_shader_source = "#version 330 core\n"
+                                   "layout (location = 0) in vec3 aPos;\n"
+                                   "layout (location = 1) in vec3 aColor;\n"
+                                   "out vec3 ourColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "   gl_Position = vec4(aPos, 1.0);\n"
+                                   "   ourColor = aColor;\n"
+                                   "}\0";
 
-const char *fragment_shader_source =
-    "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "in vec3 ourColor;"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(ourColor, 1.0);\n"
-    "}\n\0";
+const char *fragment_shader_source = "#version 330 core\n"
+                                     "out vec4 FragColor;\n"
+                                     "in vec3 ourColor;"
+                                     "void main()\n"
+                                     "{\n"
+                                     "   FragColor = vec4(ourColor, 1.0);\n"
+                                     "}\n\0";
 
 int main(int argc, char *argv[]) {
   auto engine = Engine();
@@ -39,8 +37,6 @@ int main(int argc, char *argv[]) {
 
   gl shader = renderer.make_program(vertex_shader_source,
                                     fragment_shader_source, "Demo Shader");
-
-                                    
 
   v3 vertices[4] = {{0.5f, 0.5f, 0.0f},
                     {0.5f, -0.5f, 0.0f},
