0e386e116e97973d4c6d3d0e104e78178cfe8f86
ktnlvr <artur.roos.artur@gmail.com>
2024-05-24 14:38:26 +0300

    Prune vertex and fragment shader sources from main

--- src/main.cpp
+++ src/main.cpp
@@ -12,30 +12,6 @@
 #include "engine/renderer/mesh.hpp"
 #include "engine/renderer/renderer.hpp"
 
-const char *vertex_shader_source = "#version 330 core\n"
-                                   "layout (location = 0) in vec3 aPos;\n"
-                                   "layout (location = 1) in vec3 aColor;\n"
-                                   "layout (location = 2) in vec2 aTexCoord;\n"
-                                   "out vec3 ourColor;\n"
-                                   "out vec2 TexCoord;\n"
-                                   "void main()\n"
-                                   "{\n"
-                                   "  gl_Position = vec4(aPos, 1.0);\n"
-                                   "  ourColor = aColor;\n"
-                                   "  TexCoord = aTexCoord;\n"
-                                   "}\0";
-
-const char *fragment_shader_source =
-    "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "in vec3 ourColor;"
-    "in vec2 TexCoord;"
-    "uniform sampler2D ourTexture;"
-    "void main()\n"
-    "{\n"
-    "  FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0);\n"
-    "}\n\0";
-
 using namespace engine;
 using namespace engine::renderer;
 using namespace engine::assets;
